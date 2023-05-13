c95fd6b18a0f77caf68cac2e8f2e07e089f1c31c
Andy Tang <bossbadi123@gmail.com>
2023-05-12 21:47:04 -0400

    add plan of work and copyright checklist

--- routes/doc.py
+++ routes/doc.py
@@ -3,9 +3,14 @@ from flask import send_from_directory, Blueprint
 doc = Blueprint('doc', __name__)
 
 
-@doc.route('/portfolio', methods=['GET'])
-def portfolio():
-    return send_from_directory('static', 'doc/portfolio.pdf')
+@doc.route('/student-copyright-checklist', methods=['GET'])
+def student_copyright_checklist():
+    return send_from_directory('static', 'doc/student-copyright-checklist.pdf')
+
+
+@doc.route('/plan-of-work-log', methods=['GET'])
+def plan_of_work_log():
+    return send_from_directory('static', 'doc/plan-of-work-log.pdf')
 
 
 def setup(app):
